0af45d3fcb085151b3b358e0e6f35e1bbe02e667
Johnson Zhong <johnsonzhong@hotmail.ca>
2017-05-25 00:55:58 +0200

    Add interactivity to first canvas

--- src/index.js
+++ src/index.js
@@ -7,12 +7,11 @@ import * as dp from "drawpoint";
 import './index.css';
 
 
-function fixMouseEvent(e) {
+function getMousePoint(e) {
     e = e || window.event;
     const target = e.target || e.srcElement,
         rect = target.getBoundingClientRect();
-    e.offsetX = e.clientX - rect.left;
-    e.offsetY = e.clientY - rect.top;
+    return dp.point(e.clientX - rect.left, e.clientY - rect.top);
 }
 
 // ========================================
@@ -68,15 +67,20 @@ class DemoUnit extends React.Component {
     constructor(props) {
         super(props);
 
-        const p1 = dp.point(20, 20);
-        const p2 = dp.point(170, 120);
-        p2.cp1 = dp.point(50, 50);
-        p2.cp2 = dp.point(90, 40);
 
         this.state = {
-            p1,
-            p2
+            p1 : dp.point(20, 20),
+            p2 : dp.point(170, 120),
+            cp1: dp.point(50, 50),
+            cp2: dp.point(90, 40),
         };
+
+        this.points = Object.keys(this.state);
+        console.log(this.points);
+
+        this.mousePoint = null;
+        this.movingPoint = null;
+        this.moving = false;
 
         // subsequent initialization calls in order
         // componentWillMount
@@ -92,19 +96,72 @@ class DemoUnit extends React.Component {
 
     componentDidUpdate() {
         const ctx = this.refs.canvas.getContext('2d');
-        // ctx.clearRect(0, 0, 200, 200);
-        // this.draw(ctx);
+        ctx.clearRect(0, 0, 200, 200);
+        this.draw(ctx, this.state);
     }
 
-    draw(ctx, {p1, p2}) {
+    draw(ctx, {p1, p2, cp1, cp2}) {
+        // attach the control points
+        const pp2 = dp.clone(p2);
+        pp2.cp1 = cp1;
+        pp2.cp2 = cp2;
+
         ctx.beginPath();
-        dp.drawPoints(ctx, p1, p2);
+        dp.drawPoints(ctx, p1, pp2);
         ctx.stroke();
 
         // draw the control points
-        drawControlPoints(ctx, p1, p2);
+        drawControlPoints(ctx, p1, pp2);
     }
 
+    // interaction handlers (automatically bound to this)
+    handleMouseDown = (e) => {
+        this.mousePoint = getMousePoint(e);
+
+        this.points.forEach((p) => {
+            if (dp.norm(dp.diff(this.state[p], this.mousePoint)) < 10) {
+                this.moving = true;
+                this.movingPoint = p;
+            }
+        });
+    };
+
+    handleMouseUp = () => {
+        if (this.moving === false) {
+            return;
+        }
+
+        this.refs.canvas.style.cursor = "default";
+
+        this.moving = false;
+        this.movingPoint = null;
+    };
+
+    handleMouseMove = (e) => {
+        const newMousePoint = getMousePoint(e);
+
+        let movingNearPoint = false;
+        this.points.forEach((p) => {
+            if (dp.norm(dp.diff(this.state[p], newMousePoint)) < 10) {
+                movingNearPoint = true;
+            }
+        });
+
+        this.refs.canvas.style.cursor = movingNearPoint ? "pointer" : "default";
+
+        if (this.moving === false) {
+            return;
+        }
+
+        // we want to capture relative movement to avoid instantaneous teleport
+        const movedPoint = dp.clone(this.state[this.movingPoint]);
+        const movedBy = dp.diff(this.mousePoint, newMousePoint);
+        movedPoint.x += movedBy.x;
+        movedPoint.y += movedBy.y;
+
+        this.mousePoint = newMousePoint;
+        this.setState({[this.movingPoint]: movedPoint});
+    };
 
     renderCode() {
 
@@ -112,8 +169,8 @@ class DemoUnit extends React.Component {
             <pre className="demo-code">
                 const p1 = {point(this.state.p1)};{'\n'}
                 const p2 = {point(this.state.p2)};{'\n'}
-                p2.cp1 = {point(this.state.p2.cp1)};{'\n'}
-                p2.cp2 = {point(this.state.p2.cp2)};{'\n'}
+                p2.cp1 = {point(this.state.cp1)};{'\n'}
+                p2.cp2 = {point(this.state.cp2)};{'\n'}
                 {'\n'}
                 ctx.beginPath();{'\n'}
                 dp.drawPoints(ctx, p1, p2);{'\n'}
@@ -127,6 +184,9 @@ class DemoUnit extends React.Component {
         return (
             <div className="demo-unit">
                 <canvas width="200" height="200" ref="canvas"
+                        onMouseDown={this.handleMouseDown}
+                        onMouseUp={this.handleMouseUp}
+                        onMouseMove={this.handleMouseMove}
                         className="demo-canvas"></canvas>
                 {this.renderCode()}
             </div>
