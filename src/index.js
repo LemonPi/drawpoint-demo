193b9b0fbc5b00e2cd57022f2d850f95c1447e69
Johnson Zhong <johnsonzhong@hotmail.ca>
2017-05-25 01:14:45 +0200

    Factor out basic point interaction to a factory

--- src/index.js
+++ src/index.js
@@ -62,11 +62,66 @@ function drawControlPoints(ctx, p1, p2) {
     });
 }
 
+function interactionFactory(context, pointNames) {
+    let mousePoint = null;
+    let movingPoint = null;
+    let moving = false;
+
+    return {
+        handleMouseDown (e)  {
+            mousePoint = getMousePoint(e);
+
+            pointNames.forEach((p) => {
+                if (dp.norm(dp.diff(context.state[p], mousePoint)) < 10) {
+                    moving = true;
+                    movingPoint = p;
+                }
+            });
+        },
+
+        handleMouseUp  ()  {
+            if (moving === false) {
+                return;
+            }
+
+            context.refs.canvas.style.cursor = "default";
+
+            moving = false;
+            movingPoint = null;
+        },
+
+        handleMouseMove  (e)  {
+            const newMousePoint = getMousePoint(e);
+
+            let movingNearPoint = false;
+            pointNames.forEach((p) => {
+                if (dp.norm(dp.diff(context.state[p], newMousePoint)) < 10) {
+                    movingNearPoint = true;
+                }
+            });
+
+            context.refs.canvas.style.cursor = movingNearPoint ? "pointer" : "default";
+
+            if (moving === false) {
+                return;
+            }
+
+            // we want to capture relative movement to avoid instantaneous teleport
+            const movedPoint = dp.clone(context.state[movingPoint]);
+            const movedBy = dp.diff(mousePoint, newMousePoint);
+            movedPoint.x += movedBy.x;
+            movedPoint.y += movedBy.y;
+
+            mousePoint = newMousePoint;
+            context.setState({[movingPoint]: movedPoint});
+        },
+
+    };
+}
 
 class DemoUnit extends React.Component {
     constructor(props) {
         super(props);
-
 
         this.state = {
             p1 : dp.point(20, 20),
@@ -75,12 +130,9 @@ class DemoUnit extends React.Component {
             cp2: dp.point(90, 40),
         };
 
-        this.points = Object.keys(this.state);
-        console.log(this.points);
-
-        this.mousePoint = null;
-        this.movingPoint = null;
-        this.moving = false;
+        // state holds only points (easy)
+        const points = Object.keys(this.state);
+        this.handler = interactionFactory(this, points);
 
         // subsequent initialization calls in order
         // componentWillMount
@@ -114,54 +166,6 @@ class DemoUnit extends React.Component {
         drawControlPoints(ctx, p1, pp2);
     }
 
-    // interaction handlers (automatically bound to this)
-    handleMouseDown = (e) => {
-        this.mousePoint = getMousePoint(e);
-
-        this.points.forEach((p) => {
-            if (dp.norm(dp.diff(this.state[p], this.mousePoint)) < 10) {
-                this.moving = true;
-                this.movingPoint = p;
-            }
-        });
-    };
-
-    handleMouseUp = () => {
-        if (this.moving === false) {
-            return;
-        }
-
-        this.refs.canvas.style.cursor = "default";
-
-        this.moving = false;
-        this.movingPoint = null;
-    };
-
-    handleMouseMove = (e) => {
-        const newMousePoint = getMousePoint(e);
-
-        let movingNearPoint = false;
-        this.points.forEach((p) => {
-            if (dp.norm(dp.diff(this.state[p], newMousePoint)) < 10) {
-                movingNearPoint = true;
-            }
-        });
-
-        this.refs.canvas.style.cursor = movingNearPoint ? "pointer" : "default";
-
-        if (this.moving === false) {
-            return;
-        }
-
-        // we want to capture relative movement to avoid instantaneous teleport
-        const movedPoint = dp.clone(this.state[this.movingPoint]);
-        const movedBy = dp.diff(this.mousePoint, newMousePoint);
-        movedPoint.x += movedBy.x;
-        movedPoint.y += movedBy.y;
-
-        this.mousePoint = newMousePoint;
-        this.setState({[this.movingPoint]: movedPoint});
-    };
 
     renderCode() {
 
@@ -184,9 +188,9 @@ class DemoUnit extends React.Component {
         return (
             <div className="demo-unit">
                 <canvas width="200" height="200" ref="canvas"
-                        onMouseDown={this.handleMouseDown}
-                        onMouseUp={this.handleMouseUp}
-                        onMouseMove={this.handleMouseMove}
+                        onMouseDown={this.handler.handleMouseDown}
+                        onMouseUp={this.handler.handleMouseUp}
+                        onMouseMove={this.handler.handleMouseMove}
                         className="demo-canvas"></canvas>
                 {this.renderCode()}
             </div>
