981132a65b879d60bbf5d021cd77193a46d50444
Johnson Zhong <johnsonzhong@hotmail.ca>
2017-05-29 14:33:22 +0200

    Simplify stateless component into function

--- src/Section.js
+++ src/Section.js
@@ -2,12 +2,11 @@
  * Created by Johnson on 2017-05-25.
  */
 import React from 'react';
-export default class Section extends React.Component {
-    render() {
-        return (
-            <div className="demo-section">
-                {this.props.children}
-            </div>
-        )
-    }
+
+export default function Section(props) {
+    return (
+        <div className="demo-section">
+            {props.children}
+        </div>
+    );
 }
